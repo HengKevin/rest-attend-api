0107def25e0cac83340dc87c149a35dfb553b7e9
Khefff <heng.kevin19@kit.edu.kh>
2023-01-17 00:21:05 +0700

    exclude fs

--- src/modules/attendances/attendances.service.ts
+++ src/modules/attendances/attendances.service.ts
@@ -32,7 +32,7 @@ export class AttendancesService {
   findAllByLocation(location: string) {
     return this.prisma.attendances.findMany({
       where: { location: location },
-      include: { user: true },
+      include: { user: { select: { name: true, email: true } } },
     });
   }
 
